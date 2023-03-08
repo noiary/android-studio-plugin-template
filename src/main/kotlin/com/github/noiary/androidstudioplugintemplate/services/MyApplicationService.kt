42d2758c15b81938c818d93cee03e7e5a95c33da
GitHub Action <action@github.com>
2023-03-08 02:15:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/noiary/androidstudioplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/noiary/androidstudioplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.noiary.androidstudioplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noiary.androidstudioplugintemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/noiary/androidstudioplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/noiary/androidstudioplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.noiary.androidstudioplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noiary.androidstudioplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
